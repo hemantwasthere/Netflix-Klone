c2010883e299a24b1c2fab2df24a8f374fe0b8b8
Hemant <hrsmart2150@gmail.com>
2022-07-10 17:25:22 +0530

    Fetching image for homepage

---
+++ src/components/Main.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import requests from '../Requests'
+
+const Main = () => {
+
+    const [movies, setMovies] = useState([])
+
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+
+    useEffect(() => {
+        axios.get(requests.requestPopular).then((res) => {
+            setMovies(res.data.results)
+        })
+    }, [])
+    console.log(movies)
+
+    return (
+        <div className='w-full h-[550px] text-white '>
+            <div className='w-full h-full' >
+                <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+            </div>
+        </div>
+    )
+}
+
+export default Main
