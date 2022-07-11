904273defedff4cb518ce3082e1c16cd3ee3c470
Hemant <hrsmart2150@gmail.com>
2022-07-11 17:52:23 +0530

    Added protected route to our account

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { UserAuth } from '../context/AuthContext'
+import { Navigate } from 'react-router-dom'
 
-const ProtectedRoute = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+const ProtectedRoute = ({ children }) => {
+
+    const { user } = UserAuth()
+
+    if (!user) {
+        return <Navigate to='/' />
+    } else {
+        return children
+    }
 }
 
 export default ProtectedRoute
